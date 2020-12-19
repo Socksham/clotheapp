b390dc62d505611c0ca0226027072fd797be8f36
SohamGupta21 <guptasoham@outlook.com>
2020-12-19 14:16:45 -0600

    with api results

--- clothe/main/urls.py
+++ clothe/main/urls.py
@@ -6,4 +6,6 @@ import main.views as main_views
 urlpatterns = [
     path('', main_views.home, name="home"),
     path('signup', main_views.signup_view, name="signup_view"),
+    path('preferences', main_views.preferences, name="preferences"),
+    path('recommendation', main_views.userChoices, name="recommendation")
 ]
--- clothe/main/views.py
+++ clothe/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import signupForm
 from django.contrib.auth import authenticate, login
-
+from .utils import *
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -23,3 +23,17 @@ def signup_view(request):
         form = signupForm()
 
     return render(request, 'signup.html', {'form': form})
+
+def preferences(request):
+    return render(request, "preferences.html")
+def userChoices(request):
+    location = request.POST['location']
+    gender = request.POST['gender']
+    color_ = request.POST['color']
+    clothing_type = request.POST['clothing_type']
+    style = request.POST['style']
+    upper = int(request.POST['upper'])
+    lower = int(request.POST['lower'])
+    search_results = getAPICall(location, gender, color_, clothing_type, style, upper, lower)
+    print(search_results)
+    return render(request, 'search_results.html', {'search_results':search_results})
