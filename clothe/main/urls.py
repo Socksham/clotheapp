268f2b918cd16a906688aa8e68292d0bd1dbaa4c
Socksham <60550018+Socksham@users.noreply.github.com>
2020-12-19 21:56:02 -0600

    dislike and like working

--- clothe/main/urls.py
+++ clothe/main/urls.py
@@ -4,10 +4,11 @@ import main.views as main_views
 
 urlpatterns = [
     path('', main_views.home, name="home"),
-    path('signup', main_views.signup_view, name="signup_view"),
-    path('profile', main_views.profile_view, name="profile_view"),
-    path('preferences', main_views.preferences, name="preferences"),
-    path('recommendation', main_views.userChoices, name="recommendation"),
-    path('posts', main_views.post_comment_create_and_list_view, name="posts"),
-    path('liked', main_views.like_unlike_post, name = "like_posts"),
+    path('signup/', main_views.signup_view, name="signup_view"),
+    path('profile/', main_views.profile_view, name="profile_view"),
+    path('preferences/', main_views.preferences, name="preferences"),
+    path('recommendation/', main_views.userChoices, name="recommendation"),
+    path('posts/', main_views.post_comment_create_and_list_view, name="posts"),
+    path('liked/', main_views.like_unlike_post, name = "like_posts"),
+    path('disliked/', main_views.dislike_undislike_post, name = "dislike_posts"),
 ]
