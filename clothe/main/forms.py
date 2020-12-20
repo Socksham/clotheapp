43967ae8be2aef5597d75f474160af3075e6dc13
Socksham <60550018+Socksham@users.noreply.github.com>
2020-12-19 23:55:11 -0600

    create post no css

--- clothe/main/forms.py
+++ clothe/main/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.contrib.postgres import fields
 from .admin import UserCreationForm
-from .models import User
+from .models import *
 
 class signupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'gender', 'username', 'password1', 'password2')
+
+class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'content'}))
+    class Meta:
+        model = Post
+        fields = ('content', 'image')
